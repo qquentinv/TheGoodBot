d88c3996d93e69eb9cc14f10e7247a046638b7bd
qquentinv <quentinvoisin00@gmail.com>
2024-09-08 22:28:20 +0200

    chore(prettier): code style

--- src/messages/launchStream.js
+++ src/messages/launchStream.js
@@ -1,6 +1,11 @@
 import { escapeUnderscore } from "./utils.js";
 
-export function notifyStreamStart(client, streamer, streamChannelName, streamStatus) {
+export function notifyStreamStart(
+  client,
+  streamer,
+  streamChannelName,
+  streamStatus
+) {
   console.log(`notification de live pour ${streamer} : stream started`);
   const channel = client.channels.cache.find(
     (ch) => ch.name === streamChannelName
@@ -8,7 +13,9 @@ export function notifyStreamStart(client, streamer, streamChannelName, streamSta
   if (channel) {
     const escapedStreamername = escapeUnderscore(streamer);
     channel.send(
-      `🚀 @everyone ${escapedStreamername} vient de commencer un stream sur ${streamStatus[streamer + "_" + "category"]} ! Regardez-le ici: https://www.twitch.tv/${escapedStreamername}`
+      `🚀 @everyone ${escapedStreamername} vient de commencer un stream sur ${
+        streamStatus[streamer + "_" + "category"]
+      } ! Regardez-le ici: https://www.twitch.tv/${escapedStreamername}`
     );
   }
 }
