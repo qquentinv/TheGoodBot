4d68eb2ae9fb9a35ebed6b67cfb6d49a40fe009d
Quentin <contact@yovach.dev>
2024-09-10 21:19:30 +0200

    chore: add eslint-plugin-prettier plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 ];
