f80b0d13fbf2f439bb9c587aa764d3c97ecba72d
Quentin <contact@yovach.dev>
2024-09-10 19:31:14 +0200

    chore: add prettier and eslint

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+    arrowParens: "always",
+    bracketSameLine: true,
+    semi: true,
+    trailingComma: "all",
+    singleQuote: false,
+    bracketSpacing: true,
+}
