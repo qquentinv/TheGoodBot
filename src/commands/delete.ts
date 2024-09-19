b88e4bf06781f31f8ceeb43d3bd4ab0a7e4de6dc
qquentinv <quentinvoisin00@gmail.com>
2024-09-19 22:41:40 +0200

    fix(ts): array string replace any type

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -6,11 +6,16 @@ import {
 import { wrongUsage } from "../messages/utils.ts";
 import { isStreamerExist, removeStreamer } from "../services/database.ts";
 
-export function deleteCommand(client: Client, stdin: any[]): void      {
-  if (stdin.length == 1) {
+export function deleteCommand(client: Client, stdin: string[]): void {
+  if (stdin.length == 1 && stdin[0]) {
     wrongUsage(client, stdin[0]);
     return;
   }
+
+  if (!stdin[1]) {
+    return;
+  }
+
   if (isStreamerExist(stdin[1])) {
     removeStreamer(stdin[1]);
     successfullyRemoveStreamer(client, stdin[1]);
